1590dacfb6efef23f180e8ae1ecf626289ef2218
akio0911 <akio0911@gmail.com>
2023-07-13 22:16:23 +0900

    feedback

--- miurakadai13/ViewController.swift
+++ miurakadai13/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
+struct ChecklistItem {
+    let name: String
+    let isChecked: Bool
+}
+
 class ViewController: UIViewController, UITableViewDataSource {
     //アウトレットの作成
     @IBOutlet private weak var fruitsTableView: UITableView!
     //セルの配列を作成
-    let fruits = ["りんご", "みかん", "バナナ", "パイナップル"]
-    let checkedFruits = ["みかん", "パイナップル"]
-    
+    private let fruits = [
+        ChecklistItem(name: "りんご", isChecked: false),
+        ChecklistItem(name: "みかん", isChecked: true),
+        ChecklistItem(name: "バナナ", isChecked: false),
+        ChecklistItem(name: "パイナップル", isChecked: true),
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // テーブルビューのデータソースを設定
@@ -28,11 +37,11 @@ class ViewController: UIViewController, UITableViewDataSource {
     //セルに配列の値を表示
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FruitTableViewCell
-        cell.fruitLabel.text = fruits[indexPath.row]
+        cell.fruitLabel.text = fruits[indexPath.row].name
         
         /*checkedFruits配列内にfruits配列と一致しているものがあるかどうか判定
          一致している場合はチェックマーク、そうでない場合は透明の画像を渡す*/
-        if checkedFruits.contains(fruits[indexPath.row]){
+        if fruits[indexPath.row].isChecked {
             cell.checkmarkImageView.image = UIImage(systemName: "checkmark")
         } else {
             cell.checkmarkImageView.image = UIImage(named: "transparent")
